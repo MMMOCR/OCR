94c3ab45dc9a8a2df14cec361734cdb26d1b20fb
FormaBot <formabot@interieur.gouv.fr>
2022-09-22 16:07:21 +0000

    apply clang-format

--- utils/imageutils.h
+++ utils/imageutils.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <SDL2/SDL.h>
 
-SDL_Surface* load_image(char file[]);
-void rotate_image(SDL_Surface *image , double angle);
+SDL_Surface*
+load_image(char file[]);
+void
+rotate_image(SDL_Surface* image, double angle);
