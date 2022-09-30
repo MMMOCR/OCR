215903294260d71ab5bb26fb0e3f3e4243bf529d
FormaBot <formabot@interieur.gouv.fr>
2022-09-30 16:20:02 +0000

    apply clang-format

--- solver/solver.c
+++ solver/solver.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
 #include <err.h>
+#include <stdio.h>
 
 int
 main(int argc, char const *argv[])
 {
-    if (argc != 1)
-        errx(1," ");
-    printf("oy\n");
-    return 0;
+  if (argc != 1) errx(1, " ");
+  printf("oy\n");
+  return 0;
 }
