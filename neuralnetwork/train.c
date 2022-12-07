e1730b1ed8ef1c483465ac3a6b167a9761d9eda0
Mickael <mickael.razzouk@gmail.com>
2022-12-07 18:30:05 +0100

    softmax 100% OK

--- neuralnetwork/train.c
+++ neuralnetwork/train.c
@@ -18,7 +18,7 @@ forward_propagation(struct training *t, int input)
             activation += t->training_inputs[input * INPUT_COUNT + k] *
               t->nn.hidden_weights[k * t->hidden_count + j];
         }
-
+        
         t->hidden_layer[j] = relu(activation);
     }
 
@@ -31,6 +31,7 @@ forward_propagation(struct training *t, int input)
         }
 
         t->output_layer[j] = activation;
+        
     }
 
     softmax(t->output_layer, OUTPUT_COUNT);
