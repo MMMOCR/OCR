83c7a1675d4f1fde50f9d0eea728f7275e1ed5cf
FormaBot <formabot@interieur.gouv.fr>
2022-12-11 13:46:15 +0000

    apply clang-format

--- utils/resize.c
+++ utils/resize.c
@@ -4,12 +4,13 @@
 #include <libgen.h>
 
 void
-resize_to_image(SDL_Surface* surface,int w, int h, char* path){
-    SDL_Surface* out = SDL_CreateRGBSurface(0, surface->w,
-        surface->h, 32, 0, 0, 0, 0);
+resize_to_image(SDL_Surface* surface, int w, int h, char* path)
+{
+    SDL_Surface* out =
+      SDL_CreateRGBSurface(0, surface->w, surface->h, 32, 0, 0, 0, 0);
     // erode(surface->pixels, out->pixels, 2, out->w, out->h);
-    
-    dilate(surface->pixels, out->pixels, 1, out->w, out->h); 
+
+    dilate(surface->pixels, out->pixels, 1, out->w, out->h);
 
     SDL_Rect src = { 0, 0, w, h };
     SDL_Surface* cropped = SDL_CreateRGBSurface(0, w, h, 32, 0, 0, 0, 0);
