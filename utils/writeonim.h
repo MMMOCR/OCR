546cc20f2c40d68cb41614afb9203205d0dade0d
FormaBot <formabot@interieur.gouv.fr>
2022-12-11 16:06:27 +0000

    apply clang-format

--- utils/writeonim.h
+++ utils/writeonim.h
@@ -1,21 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
 #include <err.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int
-extract_path(char *path, int *board)
-{};
+extract_path(char* path, int* board){};
 
 SDL_Surface*
-load_image(const char* path)
-{};
+load_image(const char* path){};
 
-void apply_surface( int x, int y, SDL_Surface* source, SDL_Surface* destination ) {};
+void
+apply_surface(int x, int y, SDL_Surface* source, SDL_Surface* destination){};
 
-void WriteOnIm(char *path, char *path2, char* empty, char* solved)
-{};
-
+void
+WriteOnIm(char* path, char* path2, char* empty, char* solved){};
